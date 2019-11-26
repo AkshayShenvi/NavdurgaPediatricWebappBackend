9c6c29b23e76b66303b265d381adc70cc441b82b
Akshay <akshayshenvi@gmail.com>
2019-11-26 10:54:12 -0600

    Updating the folder structure

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
-
-
+var con = require('mssql')
+var config ={
+  user:'',
+  password:'',
+  server:'LAPTOP-4409GM8C\SQLEXPRESS',
+  database:'navdurga'
+}
+con.connect(config,err=>{
+  if(err){
+    console.log(err)
+  } 
+})
 app.get('/',(req,res)=>{
+  
   res.send({hi:'there'});
 });
 
