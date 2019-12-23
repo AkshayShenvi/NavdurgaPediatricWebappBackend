4b904d0819181108414b4536b6993306a28d8960
Yugesha Sapte <yugeshasapte.4@gmail.com>
2019-12-23 01:20:32 -0600

    updated framework. added api for treatments

---
+++ app/models/db.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+process.env.NODE_ENV = 'production';
+const config = require('../../config.json');
+
+
+var connection = mysql.createConnection({
+  host: config.production.host,
+  user: config.production.user,
+  password: config.production.password
+});
+
+
+exports.getConnection = (req, res) => {
+  connection.connect(function (error) {
+    if (!!error) {
+      res.status(500).send({
+        message: error.message || "Some error occurred while connecting to database!."
+      });
+    } else {
+
+      res.send({
+        message: "Connected"
+      });
+    }
+  });
+};
+module.exports = connection;
