f42579e204151890f917727354d7481f34d4f151
Akshay <akshayshenvi@gmail.com>
2020-01-13 01:30:35 +0530

    Fininshed Doctors Table backend

--- app/routes/route.js
+++ app/routes/route.js
@@ -10,31 +10,60 @@ const treatment = require('../controllers/treatmentController.js');
 const connection = require('../../app/models/db.js');
 const user = require('../controllers/userController.js');
 const appointmentController = require('../controllers/appointmentController');
+const doctorController = require('../controllers/doctorController');
+
+//**************Doctors****************
+//Get all doctors
+app.get('/getalldoctors', doctorController.getAll)
+
+// Add Doctor 
+app.post('/adddoctors',function (req,res){
+    doctorController.addDoctor(req,res);
+})
+
+// Search Doctors
+app.post('/searchdoctors',function(req,res){
+    doctorController.searchDoctor(req,res);
+})
+
+// Update Doctors
+app.put('/doctor/:doctorId',function (req,res){
+    doctorController.updateDoctor(req,res);
+})
+
+// Delete Doctors
+app.delete('/doctors/:doctorId', function (req,res){
+    doctorController.deleteDoctor(req,res);
+})
 
 
-// get doctors with appointment count
+
+// ******************Appointments**********************
+// Get doctors with appointment count- inp{startDate,endDate}, opt{DoctorName,Count}
 app.post('/appointmentcount',(req,res)=>{
     appointmentController.getDoctorCount(req,res);
 });
-
+// Get Details of Appointments
 app.post('/appointmentdetails',(req,res)=>{
     appointmentController.getAppointmentDetails(req,res);
 });
 
+// Add New Appointment
 app.post('/addappointment',(req,res)=>{
     appointmentController.addAppointmentExistingPatient(req,res);
 });
 
 
-// add treatment
+//********************Treatments*************************
+// Add new treatment
 app.post('/treatment', function (req, res) {
     treatment.add(req, res);
 });
 
-// list all treatments
+// List all treatments
 app.get('/treatments', treatment.findAll);
 
-//search for treatment with id
+//Search for treatment with id
 app.get('/treatments/search/:treatmentId', function (req, res) {
     treatment.search(req, res);
 });
@@ -49,7 +78,10 @@ app.delete('/treatment/:treatmentId', function (req, res) {
     treatment.delete(req, res);
 });
 
-// ------------User Routing-----------
+
+
+
+// *************User Routing**************
 app.post('/login', (req, res) => {
     user.search(req, res);
 });
