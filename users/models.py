39988c1139848bea856fc9ab37cb861b06dc7e72
Akshay <akshayshenvi@gmail.com>
2021-01-03 00:38:07 -0600

    Commiting Django Backend Code

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+# Create your models here.
+
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+# Fields
+
+# emailid
+# username
+# Fname
+# LName
+# password
+# type
+# phn number
+# address
+# class User(models.Model):
